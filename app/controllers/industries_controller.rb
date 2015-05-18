70eb44842a7452110a0023f84ba04d6f0fa07dc1
Alexandra Hanley <alexandrahanley@gmail.com>
2015-05-18 15:22:58 -0600

    add all actions to industries controller

--- app/controllers/industries_controller.rb
+++ app/controllers/industries_controller.rb
@@ -1,30 +1,49 @@
 class IndustriesController < ApplicationController
 
   def index
-    @company = Company.find(params[:company_id])
-    @industries = @company.industries.all
-  end
-
-  def new
-    @industry = Industry.new
+    @industries = Industry.all
+    @companies = @industries.companies.all
   end
 
   def show
+    @company = Company.find(params[:company_id])
     @industry = Industry.find(params[:id])
+    @industry.company_id = @company.id
   end
 
   def create
+    @company = Company.find(params[:company_id])
     @industry = Industry.new(industry_params)
+    @industry.company_id = @company.id
       if @industry.save
         redirect_to industry_path(@industry), notice: "Industry was successfully created!"
       else
-        render :new
+      render :new
+      end
+    end
+
+    def edit
+      @company = Company.find(params[:company_id])
+      @industry = Industry.find(params[:id])
+    end
+
+  def update
+    @company = Company.find(params[:company_id])
+    @industry = Industry.find(params[:id])
+      if @industry.update(industry_params)
+        redirect_to industry_path(@industry)
+      else
+        render :edit
     end
   end
 
-
-
-
+  def destroy
+    @company = Company.find(params[:company_id])
+    @industry = Industry.find(params[:id])
+    @industry.company_id = @company.id
+    @industry.destroy
+    redirect_to industry_path(@industry), notice:'Industry was deleted!'
+  end
 
   private
 
