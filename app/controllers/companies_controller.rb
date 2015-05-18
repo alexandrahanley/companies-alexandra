30dd43b583212b7d3b0245905135e229bb45d8b3
Alexandra Hanley <alexandrahanley@gmail.com>
2015-05-18 15:22:11 -0600

    add all actions to companies controller

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,11 +1,46 @@
 class CompaniesController < ApplicationController
 
   def index
-    @companies = Company.all
+    @industry = Industry.find(params[:industry_id])
+    @companies = @industry.companies.all
+  end
+
+  def create
+    @industry = Industry.find(params[:industry_id])
+    @company = Company.new(company_params)
+    @company.industry_id = @industry.id
+      if @company.save
+        redirect_to company_path(@company), notice: "Company was successfully created!"
+      else
+      render :new
+      end
+    end
+
+    def edit
+      @industry = Industry.find(params[:industry_id])
+      @company = Company.find(params[:id])
+    end
+
+  def update
+    @industry = Industry.find(params[:industry_id])
+    @company = Company.find(params[:id])
+      if @company.update(company_params)
+        redirect_to company_path(@company)
+      else
+        render :edit
+    end
+  end
+
+  def destroy
+    @industry = Industry.find(params[:industry_id])
+    @company = Company.find(params[:id])
+    @company.industry_id = @industry.id
+    @company.destroy
+    redirect_to company_path(@company), notice:'Company was successfully created!'
   end
 
   private
-    def companies_params
+    def company_params
       params.require(:company).permit(:name, :description, :industry_id, :created_at, :updated_at)
     end
 
